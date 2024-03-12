b48644700acb0ff39435ef1cd4e2ad7ae1e2d1f9
unknown <kaiden.thrailkill@gmail.com>
2024-03-11 22:00:13 -0400

    Fix CI checks
    Removed unused imports, changed import with asterisk to direct import

--- code/JUnit Sample/src/test/java/edu/westga/comp4420/junit_sample/test/model/telephone_checker/TestIdentifySpam.java
+++ code/JUnit Sample/src/test/java/edu/westga/comp4420/junit_sample/test/model/telephone_checker/TestIdentifySpam.java
@@ -1,13 +1,20 @@
 package edu.westga.comp4420.junit_sample.test.model.telephone_checker;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.Random;
-
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import edu.westga.comp4420.junit_sample.model.TelephoneChecker;
 
 class TestIdentifySpam {
 
+    @ParameterizedTest
+    @CsvSource({"1111111111, false", "9990000000, true", "9000000000, true", "1234567890, false", "8889999999, true", "8000000000, true", "9119999999, true", "4119999999, true"}) 
+    void test(long number, boolean expectedResult) {
+        TelephoneChecker checker = new TelephoneChecker();
+        
+        boolean result = checker.identifySpam(number);
+        
+        Assertions.assertEquals(expectedResult, result, "checking if (" + number + ") is potential spam");
+    }
 }
